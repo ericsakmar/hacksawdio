b091f3ebcad88e4b4c287143f5b9d3ff2da06f9d
Eric Sakmar <eric.sakmar@gmail.com>
2025-08-06 08:05:32 -0400

    little refactor

---
+++ src/features/home/useSearch.ts
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { AlbumSearchResponse } from "../auth/types";
+import { invoke } from "@tauri-apps/api/core";
+
+const limit = 50;
+
+export function useSearch() {
+  const [search, setSearch] = useState("");
+  const [offset, setOffset] = useState(0);
+  const [results, setResults] = useState<AlbumSearchResponse | null>(null);
+  const [isSearching, setIsSearching] = useState(false);
+
+  const executeSearch = async (newOffset: number) => {
+    setIsSearching(true);
+
+    const res = await invoke<AlbumSearchResponse>("search_albums", {
+      search,
+      limit,
+      offset: newOffset,
+    });
+
+    setResults(res);
+    setOffset(newOffset);
+
+    if (res.items.length > 0) {
+      setFocusedAlbumId(res.items[0].id);
+    } else {
+      setFocusedAlbumId(null);
+    }
+
+    setIsSearching(false);
+  };
+
+  const setDownloaded = (id: string, downloaded: boolean) => {
+    setResults((prev) => {
+      if (!prev) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        items: prev.items.map((item) =>
+          item.id === id ? { ...item, downloaded } : item
+        ),
+      };
+    });
+  };
+
+  return {
+    search,
+    setSearch,
+    results,
+    isSearching,
+    executeSearch,
+    setDownloaded,
+    offset,
+    setOffset,
+    limit,
+  };
+}
